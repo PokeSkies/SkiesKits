755317544089856d05321114f1f0399b616964f6
Stampede <carterblowers01@gmail.com>
2023-11-02 13:22:02 -0500

    Added Dimension requirement

---
+++ src/main/kotlin/com/pokeskies/skieskits/config/requirements/types/internal/DimensionRequirement.kt
@@ -0,0 +1,33 @@
+package com.pokeskies.skieskits.config.requirements.types.internal
+
+import com.pokeskies.skieskits.config.Kit
+import com.pokeskies.skieskits.config.requirements.ComparisonType
+import com.pokeskies.skieskits.config.requirements.Requirement
+import com.pokeskies.skieskits.config.requirements.RequirementType
+import com.pokeskies.skieskits.data.KitData
+import com.pokeskies.skieskits.utils.Utils
+import net.minecraft.server.network.ServerPlayerEntity
+
+class DimensionRequirement(
+    type: RequirementType = RequirementType.DIMENSION,
+    comparison: ComparisonType = ComparisonType.EQUALS,
+    private val id: String = ""
+) : Requirement(type, comparison) {
+    override fun checkRequirements(player: ServerPlayerEntity, kitId: String, kit: Kit, kitData: KitData): Boolean {
+        if (!checkComparison())
+            return false
+
+        Utils.printDebug("Checking a ${type?.identifier} Requirement with id='$id': $this")
+
+        val value = id.equals(player.world.registryKey.value.toString(), true)
+        return if (comparison == ComparisonType.NOT_EQUALS) !value else value
+    }
+
+    override fun allowedComparisons(): List<ComparisonType> {
+        return listOf(ComparisonType.EQUALS, ComparisonType.NOT_EQUALS)
+    }
+
+    override fun toString(): String {
+        return "DimensionRequirement(comparison=$comparison, id='$id')"
+    }
+}
